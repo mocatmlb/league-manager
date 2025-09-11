31c9d652b68ab687d5f851c5dd23bc468d07ee96
moconnell <mike.oconnell@mlb.com>
2025-09-11 07:58:03 -0400

    Fix Top Navigation CSS, Links, and File Consistency

--- public/index.php
+++ public/index.php
@@ -34,6 +34,7 @@ $pageTitle = "Home - " . APP_NAME;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>/../assets/css/style.css" rel="stylesheet">
 </head>
 <body>
